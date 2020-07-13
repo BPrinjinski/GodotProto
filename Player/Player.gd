969c8f0354d5c96e817faf22606b9219910ea3b8
Brian <bprinjinski@gmail.com>
2020-07-12 22:57:50 -0700

    Clinging implemented(with face-away bug)

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,6 +10,8 @@ const JUMP_MAX = .16
 const JUMP_POWER = 18
 const WALLJUMP_MAX = .30
 const WALLJUMP_POWER = 80
+const CLING_RAY_X = 7
+const CLING_RAY_Y = -38.5
 
 enum {
 	IDLE,
@@ -26,6 +28,7 @@ onready var leftWallRay = $LeftWallRay
 onready var rightWallRay = $RightWallRay
 onready var groundRayLeft = $GroundRayLeft
 onready var groundRayRight = $GroundRayRight
+onready var clingRay = $ClingRay
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -39,6 +42,8 @@ var on_ground = false
 var on_wall_left = false
 var on_wall_right = false
 var can_jump = false
+var can_cling = true
+var can_cling_timer = 0
 var jump_time = 0
 var walljump_time = 0
 
@@ -50,6 +55,11 @@ func _physics_process(delta):
 	var horiz_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var jump_input = Input.is_action_pressed("ui_space")
 	
+	if(faceLeft):
+		clingRay.transform.origin = Vector2(-1 * CLING_RAY_X, CLING_RAY_Y)
+	else:
+		clingRay.transform.origin = Vector2(CLING_RAY_X, CLING_RAY_Y)
+	
 	if(on_ground && state != JUMP):
 		if(horiz_input == 0):
 			state = IDLE
@@ -60,7 +70,9 @@ func _physics_process(delta):
 		else:
 			state = MOVE
 	else:
-		if((on_wall_left || on_wall_right) && state != JUMP):
+		if((on_wall_left || on_wall_right) && state == FALL && can_cling && !clingRay.is_colliding()):
+			state = CLING
+		elif((on_wall_left || on_wall_right) && state != JUMP && state != CLING):
 			state = SLIDE
 		elif(velocity.y > 0):
 			state = FALL
@@ -117,10 +129,16 @@ func _physics_process(delta):
 				animationState.travel("JumpLeft")
 			else:
 				animationState.travel("JumpRight")
+		CLING:
+			cling_state(delta)
+			if(faceLeft):
+				animationState.travel("ClingLeft")
+			else:
+				animationState.travel("ClingRight")
 	
 	if(velocity.y > TERM_VELOCITY):
 		velocity.y = TERM_VELOCITY
-	elif(!on_ground && state != SLIDE):
+	elif(!on_ground && state != SLIDE && state != CLING):
 		velocity.y = (velocity.y + (GRAVITY * delta))
 	
 	velocity = move_and_slide(velocity, Vector2(0,-1))
@@ -131,6 +149,9 @@ func _physics_process(delta):
 	var groundDist = 0
 	var rayOrigin = 0
 	
+	# Because the collision object is smaller during jump and fall states, when landing,
+	# we need to manually translate the object up and transition to the idle state
+	# to avoid "pogo" behavior.
 	if(on_ground && (state == FALL || state ==  JUMP)):
 		#translate(Vector2(0, -4))
 		if(groundRayLeft.is_colliding()):
@@ -150,6 +171,11 @@ func _physics_process(delta):
 	
 	on_wall_left = leftWallRay.is_colliding()
 	on_wall_right = rightWallRay.is_colliding()
+	
+	if(!can_cling && can_cling_timer > 0):
+		can_cling_timer -= delta
+	elif(!can_cling):
+		can_cling = true
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -191,7 +217,16 @@ func walljump_state(delta):
 		walljump_time += delta
 	else:
 		state = FALL
+
+func cling_state(delta):
+	velocity.y = 0
+	if(!clingRay.is_colliding()):
+		transform.origin += Vector2(0, 1)
 	
+	if(Input.is_action_just_pressed("ui_down")):
+		state = FALL
+		can_cling = false
+		can_cling_timer = .25
 
 func apply_friction(delta):
 	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
